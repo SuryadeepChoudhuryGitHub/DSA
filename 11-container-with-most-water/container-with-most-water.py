239a23ae709ffcb26ecc900f2d2ccc349c31cfae
Suryadeep <102240688+SuryadeepChoudhuryGitHub@users.noreply.github.com>
2026-07-28 22:59:06 +0530

    Time: 126 ms (18.40%) | Memory: 20.6 MB (63.39%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def maxArea(self, height):
+        left = 0
+        right = len(height) - 1
+        max_area = 0
+
+        while left < right:
+            # calculate area
+            width = right - left
+            h = min(height[left], height[right])
+            area = width * h
+            max_area = max(max_area, area)
+
+            # move the pointer that's limiting the height
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+
+        return max_area
